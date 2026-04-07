bcaa80de54593b9e2b27bbb7b27ac0a620f3a404
SLAVALORD1 <slava.ivanov.2006@bk.ru>
2026-04-07 20:12:11 +0300

    Delete Web_Lab7 directory

--- Web_Lab7/src/components/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  message: string;
-}
-
-export const ErrorMessage = ({ message }: Props) => (
-  <div className="error">Ошибка: {message}</div>
-);
--- Web_Lab7/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import { NavMenu } from './NavMenu';
-
-export const Header = () => (
-  <div className="header">
-    <h1>Коллекция собак</h1>
-    <NavMenu />
-  </div>
-);
